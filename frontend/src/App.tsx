499e286457ad6f153e90c05b339073c1927788e6
Michał Gąsiorek <michalgasiorek@MacBook-Pro-Micha.local>
2022-04-23 12:26:32 +0200

    Setup complete

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,22 @@
-const App = () => {
+import { useState } from 'react';
+import styled from 'styled-components';
+
+function App() {
+  const [greetings] = useState('Hello World');
+
+  const Wrapper = styled.div`
+    height: 100vh;
+    width: 100vw;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  `;
+
   return (
-    <>
-      <h1>Hello World!</h1>
-    </>
+    <Wrapper>
+      <h1>{greetings}</h1>
+    </Wrapper>
   );
-};
+}
 
 export default App;
