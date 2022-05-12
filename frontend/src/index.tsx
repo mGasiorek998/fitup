d09c0a58340b6e4a3d1e3a61c878772066902548
Michał Gąsiorek <michalgasiorek@macbook-pro-micha.home>
2022-05-12 11:04:31 +0200

    FU10: Implemented global styles and theme, added routing

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,13 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router } from 'react-router-dom';
+import GlobalStyles from 'utils/GlobalStyles';
+import { ThemeProvider } from 'styled-components';
+import theme from 'utils/theme';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <GlobalStyles />
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </Router>
   </React.StrictMode>
 );
 
