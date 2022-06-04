cda02990d607f798c52d010d7bbf6c3e1bddec56
TaRaNTuLaH <czarnecki.daw@gmail.com>
2022-06-04 18:14:16 +0200

    feature/fu-38-redis: Added redis

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,12 +1,17 @@
 import { NestFactory } from '@nestjs/core';
-
+import { Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.connectMicroservice({
+    transport: Transport.REDIS,
+    url: 'redis://127.0.0.1:6379',
+  });
   app.enableCors({
     origin: 'http://localhost:3000',
   });
+  await app.startAllMicroservices();
   await app.listen(8000);
 }
 bootstrap();
